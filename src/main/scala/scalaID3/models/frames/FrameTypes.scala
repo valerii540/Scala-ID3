5e6cd2494fbed1be98a6d30a9af4c0571627ceed
valerii540 <valerii540@protonmail.com>
2020-07-14 17:56:44 +0300

    Use only frame type enum

--- src/main/scala/scalaID3/models/frames/FrameTypes.scala
+++ src/main/scala/scalaID3/models/frames/FrameTypes.scala
@@ -3,47 +3,39 @@ package scalaID3.models.frames
 object FrameTypes extends Enumeration {
   type FrameType = Value
 
-  val
-  ContentGroup, Title, Subtitle, Lead, Band, Conductor, ModifiedBy, Year, PartOfSet, TrackNum, Album, ContentType, Copyright, Bpm, Composer, Date,
-  Time, Delay, EncodedBy, Writers, AudioType, InitialKey, Languages, Length, MediaType, OriginalAlbum, OriginalFilename, OriginalWriters,
-  OriginalArtists, OriginalReleaseDate, FileOwner, Publisher, RecordingDates, InternetRadio, Picture = Value
-
-  val frameTypesDict =
-    Map(
-      ContentGroup        -> "TIT1",
-      Title               -> "TIT2",
-      Subtitle            -> "TIT3",
-      Lead                -> "TPE1",
-      Band                -> "TPE2",
-      Conductor           -> "TPE3",
-      ModifiedBy          -> "TPE4",
-      Year                -> "TYER",
-      PartOfSet           -> "TPOS",
-      TrackNum            -> "TRCK",
-      Album               -> "TALB",
-      ContentType         -> "TCON",
-      Copyright           -> "TCOP",
-      Bpm                 -> "TBPM",
-      Composer            -> "TCOM",
-      Date                -> "TDAT",
-      Time                -> "TIME",
-      Delay               -> "TDLY",
-      EncodedBy           -> "TENC",
-      Writers             -> "TEXT",
-      AudioType           -> "TFLT",
-      InitialKey          -> "TKEY",
-      Languages           -> "TLAN",
-      Length              -> "TLEN",
-      MediaType           -> "TMED",
-      OriginalAlbum       -> "TOAL",
-      OriginalFilename    -> "TOFN",
-      OriginalWriters     -> "TOLY",
-      OriginalArtists     -> "TOPE",
-      OriginalReleaseDate -> "TORY",
-      FileOwner           -> "TOWN",
-      Publisher           -> "TPUB",
-      RecordingDates      -> "TRDA",
-      InternetRadio       -> "TRSN",
-      Picture             -> "APIC"
-    )
+  val ContentGroup: FrameType        = Value("TIT1")
+  val Title: FrameType               = Value("TIT2")
+  val Subtitle: FrameType            = Value("TIT3")
+  val Lead: FrameType                = Value("TPE1")
+  val Band: FrameType                = Value("TPE2")
+  val Conductor: FrameType           = Value("TPE3")
+  val ModifiedBy: FrameType          = Value("TPE4")
+  val Year: FrameType                = Value("TYER")
+  val PartOfSet: FrameType           = Value("TPOS")
+  val TrackNum: FrameType            = Value("TRCK")
+  val Album: FrameType               = Value("TALB")
+  val ContentType: FrameType         = Value("TCON")
+  val Copyright: FrameType           = Value("TCOP")
+  val Bpm: FrameType                 = Value("TBPM")
+  val Composer: FrameType            = Value("TCOM")
+  val Date: FrameType                = Value("TDAT")
+  val Time: FrameType                = Value("TIME")
+  val Delay: FrameType               = Value("TDLY")
+  val EncodedBy: FrameType           = Value("TENC")
+  val Writers: FrameType             = Value("TEXT")
+  val AudioType: FrameType           = Value("TFLT")
+  val InitialKey: FrameType          = Value("TKEY")
+  val Languages: FrameType           = Value("TLAN")
+  val Length: FrameType              = Value("TLEN")
+  val MediaType: FrameType           = Value("TMED")
+  val OriginalAlbum: FrameType       = Value("TOAL")
+  val OriginalFilename: FrameType    = Value("TOFN")
+  val OriginalWriters: FrameType     = Value("TOLY")
+  val OriginalArtists: FrameType     = Value("TOPE")
+  val OriginalReleaseDate: FrameType = Value("TORY")
+  val FileOwner: FrameType           = Value("TOWN")
+  val Publisher: FrameType           = Value("TPUB")
+  val RecordingDates: FrameType      = Value("TRDA")
+  val InternetRadio: FrameType       = Value("TRSN")
+  val Picture: FrameType             = Value("APIC")
 }
