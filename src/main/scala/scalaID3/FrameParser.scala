9f7d8b6ee736acd015b196ef9e88d61b8ece58bb
Valerii <valerii540@protonmail.com>
2020-09-24 00:21:50 +0300

    Add all standard text encodings support

--- src/main/scala/scalaID3/FrameParser.scala
+++ src/main/scala/scalaID3/FrameParser.scala
@@ -35,7 +35,7 @@ private[scalaID3] object FrameParser {
         traverseFile(acc + (frameHeader.frameType -> (FrameWithPosition(frame, framePosition) +: acc(frameHeader.frameType))))
 
       case Unknown(id) =>
-        println(s"Unsupported frame ID type: $id")
+        println(s"From ${framePosition} unsupported frame ID type: $id")
         acc
     }
 
