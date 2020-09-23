9902fb5aa3b4e3b6290ab8bc2755971cce131e86
valerii540 <valerii540@protonmail.com>
2020-09-24 00:21:50 +0300

    Add all standard text encodings support

--- src/main/scala/scalaID3/utils/EncodingHelper.scala
+++ src/main/scala/scalaID3/utils/EncodingHelper.scala
@@ -2,7 +2,7 @@ package scalaID3.utils
 
 import java.nio.charset.{Charset, StandardCharsets}
 
-import scalaID3.utils.EncodingHelper.TextEncodings.{Encoding, Unicode, `ISO-8859-1`}
+import scalaID3.utils.EncodingHelper.TextEncodings.{Encoding, `ISO-8859-1`, `URF-16`, `UTF-16BE`, `UTF-8`}
 
 import scala.util.Try
 
@@ -10,19 +10,25 @@ object EncodingHelper {
   object TextEncodings extends Enumeration {
     type Encoding = Value
     val `ISO-8859-1`: Encoding = Value("ISO-8859-1")
-    val Unicode: Encoding      = Value("UTF-16")
+    val `URF-16`: Encoding     = Value("UTF-16")
+    val `UTF-16BE`: Encoding   = Value("UTF-16BE")
+    val `UTF-8`: Encoding      = Value("UTF-8")
   }
 
   def identify(byte: Byte): Try[Encoding] =
     Try(byte match {
       case 0 => `ISO-8859-1`
-      case 1 => Unicode
+      case 1 => `URF-16`
+      case 2 => `UTF-16BE`
+      case 3 => `UTF-8`
       case x => throw new Exception(s"Unknown text frame encoding: $x")
     })
 
   def standardCharset(encoding: Encoding): Charset =
     encoding match {
       case `ISO-8859-1` => StandardCharsets.ISO_8859_1
-      case Unicode      => StandardCharsets.UTF_16
+      case `URF-16`     => StandardCharsets.UTF_16
+      case `UTF-16BE`   => StandardCharsets.UTF_16BE
+      case `UTF-8`      => StandardCharsets.UTF_8
     }
 }
