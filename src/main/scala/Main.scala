3e54ad2f38705fba3059071ef7b8f1928273d19d
valerii540 <valerii540@protonmail.com>
2020-09-24 00:23:01 +0300

    Update debug staff

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,38 +4,48 @@ import scalaID3.ID3Tag
 
 import scala.util.{Failure, Success, Try}
 
-object Main extends App {
-  list()
+object Main {
+  val musicFolderPath = "/home/proton/Music"
+
+  def main(args: Array[String]): Unit =
+    if (args.length > 0 && args.head != "all") {
+      println(s"song: ${args.head}")
+
+      Try(new ID3Tag(musicFolderPath + "/" + args.head)) match {
+        case Success(id3tag) =>
+          println(s"""
+               |ID3 version: ${id3tag.header.version}
+               |
+               |${id3tag.getFrames.mkString("\n")}
+               |""".stripMargin)
+          id3tag.close()
+        case Failure(exception) =>
+          throw exception
+      }
+    } else
+      list()
 
   def list(): Unit = {
-    val musicDir = new File("/home/proton/Music")
+    val musicDir = new File(musicFolderPath)
 
     musicDir.listFiles.toList.zipWithIndex.foreach { fileAndIndex =>
-      Try (new ID3Tag(fileAndIndex._1.getAbsolutePath)) match {
+      println(s"# ${fileAndIndex._2 + 1}\nsong: ${fileAndIndex._1.getName}")
+      Try(new ID3Tag(fileAndIndex._1.getAbsolutePath)) match {
         case Success(id3tag) =>
-          println(
-            s"""
-               |# ${fileAndIndex._2 + 1}
-               |song: ${fileAndIndex._1.getName}
-               |
+          println(s"""
                |ID3 version: ${id3tag.header.version}
                |
                |${id3tag.getFrames.mkString("\n")}
                |----------------------
                |""".stripMargin)
           id3tag.close()
-        case Failure(exception: AssertionError)  =>
-          println(
-            s"""
-               |# ${fileAndIndex._2 + 1}
-               |song: ${fileAndIndex._1.getName}
-               |
+        case Failure(exception: AssertionError) =>
+          println(s"""
                |EXCEPTION: ${exception.getMessage}
                |----------------------
                |""".stripMargin)
         case Failure(exception) =>
-          println(
-            s"""
+          println(s"""
                |# ${fileAndIndex._2 + 1}
                |song: ${fileAndIndex._1.getName}
                |----------------------
