31c60a532ed849cdfea4875d64507e2d8c208022
valerii540 <valerii540@protonmail.com>
2020-10-01 23:36:55 +0300

    Add Audio encryption frame

--- src/main/scala/scalaID3/data/AllFrameTypes.scala
+++ src/main/scala/scalaID3/data/AllFrameTypes.scala
@@ -12,6 +12,7 @@ object AllFrameTypes {
       .orElse(Option.when(id == PrivateFrameType.id)(PrivateFrameType))
       .orElse(Option.when(id == MusicCDidFrameType.id)(MusicCDidFrameType))
       .orElse(Option.when(id == PopularimeterFrameType.id)(PopularimeterFrameType))
+      .orElse(Option.when(id == AudioEncryptionFrameType.id)(AudioEncryptionFrameType))
       // Non-standard frames
       .orElse(Option.when(id == NCONFrameType.id)(NCONFrameType))
       .getOrElse(Unknown(id))
